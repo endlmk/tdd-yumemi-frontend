214b1dd67681d08a52970a1c1960e670778b7212
endlmk <1269467+endlmk@users.noreply.github.com>
2023-01-25 23:47:10 +0900

    Add first e2e test for checkboxes.

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -4,18 +4,13 @@
 // https://github.com/cypress-io/eslint-plugin-cypress
 
 describe('Navigation', () => {
-  it('should navigate to the about page', () => {
+  it('should show prefecture checkboxes', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
 
-    // Find a link with an href attribute containing "about" and click it
-    cy.get('a[href*="about"]').click()
+    cy.get('[type="check"]').should('have.length', 1)
 
-    // The new url should include "/about"
-    cy.url().should('include', '/about')
-
-    // The new page should contain an h1 with "About page"
-    cy.get('h1').contains('About Page')
+    cy.get('[type="check"]').first().contains('北海道')
   })
 })
 
